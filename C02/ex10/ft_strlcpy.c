192e5246f7cfec00ab8f285a57ae98a37217cc66
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-07 20:13:48 +0200

    Add files via upload

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbotuna <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 18:40:59 by dbotuna           #+#    #+#             */
+/*   Updated: 2022/09/07 18:41:05 by dbotuna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (size == 0)
+		return (i);
+	i = 0;
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
