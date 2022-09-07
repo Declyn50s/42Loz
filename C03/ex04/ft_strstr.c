042f72d0aefb24d6cbe206e200e5d6bd4801063d
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-07 20:24:07 +0200

    Add files via upload

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbotuna <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 20:22:09 by dbotuna           #+#    #+#             */
+/*   Updated: 2022/09/07 20:22:31 by dbotuna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		if (str[i] == to_find[j])
+		{
+			while (str[i + j] == to_find[j])
+			{
+				j++;
+				if (!to_find[j])
+					return (&str[i]);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
