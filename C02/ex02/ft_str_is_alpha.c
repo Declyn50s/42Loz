db011e94dc25a5763f5a3e64c7af117afca03e29
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-15 16:34:45 +0200

    Update ft_str_is_alpha.c

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -28,3 +28,21 @@ int	ft_str_is_alpha(char *str)
 		return (0);
 	}
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "abcdefghijklmnopqrstuvwxyz";
+	char c[] = "contientPasSeuLementDesLettres[";
+	char d[] = "0contientpasSeuLementDeLettres";
+	char e[] = "";
+
+	printf("%d\n", ft_str_is_alpha(a));
+	printf("%d\n", ft_str_is_alpha(b));
+	printf("%d\n", ft_str_is_alpha(c));
+	printf("%d\n", ft_str_is_alpha(d));
+	printf("%d\n", ft_str_is_alpha(e));
+	return (0);
+}*/
