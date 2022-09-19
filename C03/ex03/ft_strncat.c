fd44ee680cc3ad4bf82e111846130722b937c302
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-19 17:51:07 +0200

    Update ft_strncat.c

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -30,3 +30,19 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	dest[i] = '\0';
 	return (dest);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+
+int	main(void)
+{
+	char	dest[50] = "bonjour";
+	char	*src;
+	unsigned int	size;
+
+	size = 8;
+	src = " les amis";
+	printf(".%s.\n", strncat(dest, src, size));
+}*/
