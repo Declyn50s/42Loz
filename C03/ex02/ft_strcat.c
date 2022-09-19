0dfe43134e2fd9566a26305ea0cc7d8695e8fc7d
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-19 17:50:16 +0200

    Update ft_strcat.c

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -30,3 +30,18 @@ char	*ft_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src);
+
+int	main(void)
+{
+	char	dest[50] = "bonjour";
+	char	*src;
+
+	src = " les amis";
+	printf(".%s.\n", ft_strcat(dest, src));
+	printf(".%s.\n", strcat(dest, src));
+}*/
