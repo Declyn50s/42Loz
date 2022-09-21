b52a2d7a8d2bbacc07586292cd600f0bfe8c0a2c
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2022-09-21 11:55:55 +0200

    Update aff_a.c

--- 42Exam/00-aff_a/aff_a.c
+++ 42Exam/00-aff_a/aff_a.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
 
-int   main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int	i;
-	
+
 	i = 0;
 	if (argc != 2)
-	  write(1, "a", 1);
-  else
-  {
-	  while(argv[1][i])
-			  { 
-        if(argv[1][i] == 'a') 
+		write(1, "a", 1);
+	else
+	{
+		while (argv[1][i])
 		{
-          write(1, "a", 1);
-		  break ;
+			if (argv[1][i] == 'a')
+			{
+				write(1, "a", 1);
+			}
+			i += 1;
 		}
-        i += 1;     
 	}
-  }
-  write(1, "\n", 1);
-  return (0);
+	write(1, "\n", 1);
+	return (0);
 }
