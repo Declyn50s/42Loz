7a22d3a9a65f26b168e42a667772b4759f1abbc8
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2023-03-17 16:31:52 +0100

    Update ft_is_negative.c

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -30,3 +30,12 @@ int	main(void)
 {
 	ft_is_negative(0);
 }*/
+
+/*
+Explications :
+La fonction ft_is_negative() prend un entier en paramètre et utilise une instruction if pour vérifier si cet entier est négatif.
+Si l'entier est négatif, la fonction write() est utilisée pour écrire le caractère 'N' sur la sortie standard (stdout) pour indiquer un nombre négatif.
+Sinon, la fonction write() est utilisée pour écrire le caractère 'P' sur la sortie standard (stdout) pour indiquer un nombre positif ou nul.
+À la fin de la fonction, la fonction write() est utilisée pour ajouter un caractère de saut de ligne (\n) pour placer le résultat sur une nouvelle ligne.
+La fonction main() est incluse pour appeler la fonction ft_is_negative() avec des exemples d'entiers positifs, nuls et négatifs et exécuter le programme.
+*/
