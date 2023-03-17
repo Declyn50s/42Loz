d1bab5438d389189e82b56763c821a9d010734c6
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2023-03-17 15:59:58 +0100

    Update ft_putchar.c

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -21,3 +21,9 @@ int	main(void)
 {
 	ft_putchar('c');
 }*/
+
+/* 
+Ce code est en C et il inclut la bibliothèque standard "unistd.h" qui fournit des fonctions pour effectuer des opérations sur les fichiers, les entrées/sorties et le système d'exploitation.
+La fonction "ft_putchar" prend un caractère (c) en entrée et l'écrit dans la sortie standard (STDOUT) en utilisant la fonction "write". Cette dernière prend en entrée le descripteur de fichier "1" (qui représente la sortie standard), l'adresse du caractère (c) et la taille de la donnée à écrire qui est égale à 1.
+En somme, cette fonction permet d'écrire un caractère sur la sortie standard, ce qui peut être utile pour afficher des messages à l'utilisateur ou pour effectuer un débogage.
+*/
