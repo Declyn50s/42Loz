fb832ec841f98b61b2904815afe248676f9c210c
Declyn50s <93099544+Declyn50s@users.noreply.github.com>
2023-03-17 15:56:02 +0100

    Update ft_strlen.c

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -19,3 +19,14 @@ int	ft_strlen(char *str)
 		n++;
 	return (n);
 }
+
+/*
+Ce code est une fonction en langage C appelée "ft_strlen" qui calcule la longueur d'une chaîne de caractères. Voici ce qu'il fait étape par étape :
+
+1. La fonction prend en entrée un pointeur vers une chaîne de caractères (char* str).
+2. La variable entière n est initialisée à 0.
+3. La boucle while parcourt chaque caractère de la chaîne de caractères jusqu'à ce qu'elle atteigne le caractère nul ('\0'), qui marque la fin de la chaîne. À chaque tour de boucle, la variable n est incrémentée de 1.
+4. Une fois la fin de la chaîne atteinte, la fonction retourne la valeur de n, qui représente la longueur de la chaîne (hors le caractère nul de fin).
+
+Par exemple, si la chaîne de caractères passée en entrée est "Hello world", la fonction va parcourir chaque caractère de la chaîne et incrémenter n jusqu'à atteindre la fin de la chaîne (caractère nul), puis elle retournera la valeur 11 correspondant au nombre de caractères dans la chaîne.
+*/
